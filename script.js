e1fe1f4a3671fe2f82f18876e88c6844e97a5bac
Shalakushka <56279749+Shalakushka@users.noreply.github.com>
2024-06-21 16:07:26 +0200

    Etch-A-Sketch functionality implemented
    This commit marks the initial version of the
    Etch-A-Sketch project. The core functionality
    has been implemented, allowing users to draw by
    hovering over a grid of squares.
    This version replicates the basic features of a
    traditional Etch-A-Sketch toy.

---
+++ script.js
@@ -0,0 +1,23 @@
+const board = document.querySelector(".board");
+
+function createCells(nb, size) {
+    for (let i = 0; i < nb; i++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        cell.style.cssText = `height: ${size}px; width: ${size}px;`
+        board.appendChild(cell);
+    }
+}
+
+function changeCellColor(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+createCells(256, 32);
+let cells = document.querySelectorAll(".cell");
+console.log(cells.length);
+console.log(cells);
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("mouseenter", (e) => changeCellColor(e));
+}
+
